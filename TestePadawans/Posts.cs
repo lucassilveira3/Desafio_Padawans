592271726e72dedd0a278d0df34f64a5bc2ef334
Lucas Silveira <50899678+lucassilveira3@users.noreply.github.com>
2021-09-12 18:58:10 -0300

    Add files via upload

---
+++ TestePadawans/Posts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Net.Http;
+
+namespace TestePadawans
+{
+    public partial class Posts : Form
+    {
+        public Posts()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HttpClient apiClient = new HttpClient();
+            apiClient.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+            HttpResponseMessage response = apiClient.GetAsync(requestUri: "posts").Result;
+            var post = response.Content.ReadAsAsync<IEnumerable<Postagens>>().Result;
+            dataGridView1.DataSource = post;
+        }
+    }
+}
---
+++ TestePadawans/ToDos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestePadawans
+{
+    class ToDos
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+    }
+}
