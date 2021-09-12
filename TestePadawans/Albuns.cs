e6db572aaa0ccef7a7053e20ee05cddd4ac1e817
Lucas Silveira <50899678+lucassilveira3@users.noreply.github.com>
2021-09-12 19:01:48 -0300

    Commit Final

---
+++ TestePadawans/Albuns.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestePadawans
+{
+    class Albuns
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+    }
+}
